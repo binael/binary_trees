1e59bf304c9ca8a0dc316377e50b3ab5105d245e
Binael <nbinael@yahoo.com>
2022-08-24 16:51:43 +0100

    tree height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,28 @@
+#include "binary_trees"
+
+/**
+* binary_tree_height - function that measures the height of a binary tree
+* @tree: the node to measure from
+*
+* Return: height
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_node, right_node;
+
+	if (tree == NULL || (tree->left NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+	left_node = binary_tree_height(tree->left) + 1;
+	right_node = binary_tree_height(tree->right) + 1;
+
+	if (left_node >= right_node)
+	{
+		return (left_node);
+	}
+	else
+	{
+		return (right_node);
+	}
+}
