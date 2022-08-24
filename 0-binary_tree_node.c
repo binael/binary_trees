41e9edf0f49907f6ece3f76660c122bd6f1884a8
Binael <nbinael@yahoo.com>
2022-08-24 14:22:32 +0100

    corrections

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,14 +9,19 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	parent = malloc(sizeof(binary_tree_t));
-	if (parent == NULL)
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	parent->n = value;
-	parent->right = NULL;
-	parent->left = NULL;
 
-	return (parent);
+	new_node->n = value;
+	new_node->right = NULL;
+	new_node->left = NULL;
+	new_node->parent = parent;
+
+	return (new_node);
 }
