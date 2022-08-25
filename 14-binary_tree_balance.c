d70cd21121b479a07bf35a356047fc3b1f2a7ba9
Binael <nbinael@yahoo.com>
2022-08-25 14:26:49 +0100

    node balance

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_balance - measures the balance factor of a binary tree
+* @tree: the node to measure the balance factor
+*
+* Return: integer balance factor
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_node, right_node;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left_node = child_node_height(tree->left);
+	right_node = child_node_height(tree->right);
+
+	return (abs(left_node - right_node));
+}
+
+
+/**
+* child_node_height - function that measures the height of a binary tree
+* @tree: the node to measure from
+*
+* Return: height
+*/
+size_t child_node_height(const binary_tree_t *tree)
+{
+	size_t left_node, right_node;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left_node = child_node_height(tree->left) + 1;
+	right_node = child_node_height(tree->right) + 1;
+
+	if (left_node >= right_node)
+	{
+		return (left_node);
+	}
+	else
+	{
+		return (right_node);
+	}
+}
--- binary_trees.h
+++ binary_trees.h
@@ -67,5 +67,6 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 int heap_extract(heap_t **root);
+size_t child_node_height(const binary_tree_t *tree);
 
 #endif /* end of header file */
