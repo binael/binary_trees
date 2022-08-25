84dbb52293e2c8b3a1220ad196862e38dbba9ecd
binael <nbinael@yahoo.com>
2022-08-25 15:04:19 -0700

    binary tree uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - the sibling of a binary tree node
+ * @node: the pointer to the node in consideration
+ *
+ * Return: The sibling node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *grand_parent, *papa;
+
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+
+	papa = node->parent;
+	grand_parent = node->parent->parent;
+
+	if (grand_parent->left == papa)
+	{
+		if (grand_parent->right)
+		{
+			return (grand_parent->right);
+		}
+	}
+	else if (grand_parent->right == node)
+	{
+		if (grand_parent->left)
+		{
+			return (grand_parent->left);
+		}
+	}
+
+	return (NULL);
+}
