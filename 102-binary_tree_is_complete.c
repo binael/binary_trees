6c18264349b472ff170a80618e5edb41d7d3b5e2
Nweke Chima Gideon <84105030+CHIMAVMV@users.noreply.github.com>
2023-03-31 04:00:52 +0100

    Update 102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -2,7 +2,8 @@
 
 levelorder_queue_t *create_node(binary_tree_t *node);
 void free_queue(levelorder_queue_t *head);
-void push(binary_tree_t *node, levelorder_queue_t *head, levelorder_queue_t **tail);
+void push(binary_tree_t *node, levelorder_queue_t *head,
+		levelorder_queue_t **tail);
 void pop(levelorder_queue_t **head);
 int binary_tree_is_complete(const binary_tree_t *tree);
 
